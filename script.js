5c76cdc2ebc359ba35f8e60565a7e4fe2898a91b
MahendraMakad <87107054+MahendraMakad@users.noreply.github.com>
2023-06-01 11:39:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,113 +1,82 @@
 // Select the input file element
-var fileInput = document.querySelector('#file-input');
-var originalHeight, originalWidth;
-var checkbox1 = document.getElementById("checkbox1");
-var checkbox2 = document.getElementById("checkbox2");
+const fileInput = document.querySelector('#file-input');
 // global variable to preserve original image data
+var originalData,originalWidth, originalHeight;
+// checkbox for auto preview
+var checkbox = document.getElementById("auto-preview-btn");
 // selecting the image canvas
-var canvas = document.getElementById('my-image');
+var canvas = document.getElementById('image-canvas');
 var ctx = canvas.getContext("2d");
+const btn1 = document.getElementById("button1");
+const btn2 = document.getElementById("button2");
 
+btn1.addEventListener('click', flipCanvasHorizontally);
 
-var container = document.getElementById("imageContainer");
-
-
-checkbox1.addEventListener("change",function() {
-    if(this.checked) {
-      flipCanvasHorizontally();
-    }
-});
-
-checkbox2.addEventListener("change",function() {
-  if(this.checked) {
-    flipCanvasVertically()
-  }
-});
+btn2.addEventListener('click', flipCanvasVertically);
 
 function flipCanvasVertically() {
-  // Create a temporary canvas to store the flipped image
-  var tempCanvas = document.createElement("canvas");
-  tempCanvas.width = canvas.width;
-  tempCanvas.height = canvas.height;
-  var tempCtx = tempCanvas.getContext("2d");
-  // Flip the image vertically
-  tempCtx.scale(1, -1);
-  tempCtx.translate(0, -canvas.height);
-  tempCtx.drawImage(canvas, 0, 0);
-  // Replace the existing canvas with the flipped image
-  canvas.width = tempCanvas.width;
-  canvas.height = tempCanvas.height;
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.drawImage(tempCanvas, 0, 0);
-}
-
-function flipCanvasHorizontally() {
-  // Create a temporary canvas to store the flipped image
-  var tempCanvas = document.createElement("canvas");
-  tempCanvas.width = canvas.width;
-  tempCanvas.height = canvas.height;
-  var tempCtx = tempCanvas.getContext("2d");
-  // Flip the image horizontally
-  tempCtx.scale(-1, 1);
-  tempCtx.translate(-canvas.width, 0);
-  tempCtx.drawImage(canvas, 0, 0);
-  // Replace the existing canvas with the flipped image
-  canvas.width = tempCanvas.width;
-  canvas.height = tempCanvas.height;
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.drawImage(tempCanvas, 0, 0);
-}
-
-
+    // Create a temporary canvas to store the flipped image
+    var tempCanvas = document.createElement("canvas");
+    console.log(canvas);
+    tempCanvas.width = canvas.width;
+    tempCanvas.height = canvas.height;
+    var tempCtx = tempCanvas.getContext("2d");
+    // Flip the image vertically
+    tempCtx.scale(1, -1);
+    tempCtx.translate(0, -canvas.height);
+    tempCtx.drawImage(canvas, 0, 0);
+    // Replace the existing canvas with the flipped image
+    canvas.width = tempCanvas.width;
+    canvas.height = tempCanvas.height;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(tempCanvas, 0, 0);
+  }
+  
+  function flipCanvasHorizontally() {
+    console.log("flipped");
+    // Create a temporary canvas to store the flipped image
+    var tempCanvas = document.createElement("canvas");
+    tempCanvas.width = canvas.width;
+    tempCanvas.height = canvas.height;
+    var tempCtx = tempCanvas.getContext("2d");
+    // Flip the image horizontally
+    tempCtx.scale(-1, 1);
+    tempCtx.translate(-canvas.width, 0);
+    tempCtx.drawImage(canvas, 0, 0);
+    // Replace the existing canvas with the flipped image
+    canvas.width = tempCanvas.width;
+    canvas.height = tempCanvas.height;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(tempCanvas, 0, 0);
+  }
 // onchage event on loading of a file which will draw image and canvas for
 // original image
 $('#file-input').change(function () {
-  var file = this.files[0];
-  var image = new Image();
+    var file = this.files[0];
+    var image = new Image();
 
-  // Load the image into the canvas and resize it
-  var reader = new FileReader();
-  reader.onload = function (event) {
-    image.src = event.target.result;
-    image.onload = function () {
-      let width, height;
-      originalHeight = image.height;
-      originalWidth = image.width;
-      if (image.width > image.height) {
-        var widthRatio = 500 / image.width;
-        width = 500;
-        height = image.height * widthRatio;
-        if (height > 500) {
-          var heightRatio = 500 / height;
-          height = 500;
-          width = width * heightRatio;
-        }
-      } else {
-        var heightRatio = 500 / image.height;
-        height = 500;
-        width = image.width * heightRatio;
-        if (width > 500) {
-          var widthRatio = 500 / width;
-          width = 500;
-          height = height * widthRatio;
-        }
-      }
-      canvas.width = width;
-      canvas.height = height;
-      console.log(width, height);
-      container.style.height = height + 10 + "px";
-      container.style.width = width + 10 + "px";
-      ctx.drawImage(image, 0, 0, width, height);
-      originalData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    // Load the image into the canvas and resize it
+    var reader = new FileReader();
+    reader.onload = function (event) {
+        image.src = event.target.result;
+        image.onload = function () {
+            originalHeight = image.height;
+            originalWidth = image.width;
+            var canvasWidth = image.width;
+            var canvasHeight = image.height;
+            if (canvasWidth > 400) {
+                canvasHeight = canvasHeight * (400 / canvasWidth);
+                canvasWidth = 400;
+            }
+            canvas.width = canvasWidth;
+            canvas.height = canvasHeight;
+            ctx.drawImage(image, 0, 0, canvasWidth, canvasHeight);
+            $('#image-preview').attr('src', canvas.toDataURL()).css('display', 'inline');
+            originalData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        };
     };
-  };
-  reader.readAsDataURL(file);
+    reader.readAsDataURL(file);
 });
-
-
-
-
-
 
 
 //add eventListner to JPG and PNG download buttons
@@ -133,8 +102,3 @@ function downloadImage(event) {
   }
   downloadLink.click();
 }
-
-
-
-
-
